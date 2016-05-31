7c44c0805f89e033d3e54ce0e968990771212b5c
Alex Jacks and Noah Yasskin <noah@>
2016-05-31 16:56:06 -0700

    creates 3 flashcards or text which hide and show via toggle method

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
+//Toggles all elements, needs to have show-hide correspondance
+
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#show-card-1").toggle();
     $("#hide-card-1").toggle();
+    $("#show-card-2").toggle();
+    $("#hide-card-2").toggle();
+    $("#show-card-3").toggle();
+    $("#hide-card-3").toggle();
   });
 });
